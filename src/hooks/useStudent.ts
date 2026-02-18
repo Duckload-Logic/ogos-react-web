d3ac0f292932e6fe628d6cb806bc4bb697cbf54c
olazo-johnalbert <jaolazo4@gmail.com>
2026-02-18 17:17:35 +0800

    feat: add useStudent hook for fetching student data

---
+++ src/hooks/useStudent.ts
@@ -0,0 +1,20 @@
+import { apiClient } from "@/lib/api";
+
+export const useStudent = () => {
+  const fetchStudentData = async (userId: string) => {
+    const { data } = await apiClient.get(`/students/${userId}`, {
+      params: {
+        include_address: true,
+        include_finance: true,
+        include_health: true,
+        include_family: true,
+        include_education: true,
+      }
+    });
+    return data;
+  }
+
+  return {
+    fetchStudentData,
+  };
+}
