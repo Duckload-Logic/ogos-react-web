5091f2f6e17610ffa164e0d545bb1ffd1ba50ef8
olazo-johnalbert <jaolazo4@gmail.com>
2026-03-03 16:28:26 +0800

    Refactor useIIRStatus hook to use iirService for fetching IIR data

--- src/features/iir/hooks/useIIRStatus.ts
+++ src/features/iir/hooks/useIIRStatus.ts
@@ -1,14 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import { checkStudentOnboardingStatus } from "../services/service";
+import { checkStudentOnboardingStatus, iirService } from "../services/service";
 
 const STUDENT_ONBOARDING_STATUS_QUERY_KEY = "studentOnboardingStatus";
 const STUDENT_ONBOARDING_STATUS_STALE_TIME = 1000 * 60 * 30; // 30 minutes
 const STUDENT_ONBOARDING_STATUS_GC_TIME = 1000 * 60 * 60 * 12; // 12 hours
 
-export function useStudentOnboardingStatus(userID: number) {
+export function useIIRStatus(userID: number) {
   return useQuery({
     queryKey: [STUDENT_ONBOARDING_STATUS_QUERY_KEY, userID],
-    queryFn: () => checkStudentOnboardingStatus(userID),
+    queryFn: () => iirService.getIIRByUserID(userID),
     enabled: !!userID,
     staleTime: STUDENT_ONBOARDING_STATUS_STALE_TIME,
     gcTime: STUDENT_ONBOARDING_STATUS_GC_TIME,
