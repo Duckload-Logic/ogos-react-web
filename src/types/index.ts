e3b13993727e2a9543cf4e6eb6d78e6ab818786c
olazo-johnalbert <jaolazo4@gmail.com>
2026-03-05 03:22:15 +0800

    refactor: remove unused global types barrel export

--- src/types/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Global Types Barrel Export
- * Central location for all cross-feature types
- */
-
-export * from "../features/users/types/user";
-export * from "./appointment";
-export * from "./student";
