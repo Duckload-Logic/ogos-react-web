3a0fe7b3019f40c0aa456057aae5a7bfe5257708
olazo-johnalbert <155875419+olazo-johnalbert@users.noreply.github.com>
2026-03-12 08:12:22 +0800

    Fix: ensure useMe hook is called with an empty object (#46)

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -17,7 +17,7 @@ import { useMe } from "@/features/users/hooks/useMe";
 export default function Login() {
   const navigate = useNavigate();
   const { login, isLoggingIn, loginError } = useLogin();
-  const { refetch } = useMe();
+  const { refetch } = useMe({});
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
